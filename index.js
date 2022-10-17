6885e17f72bfa10f136de359e7698da5b7d4f5eb
jiewoongkang <44522838+jiewoongkang@users.noreply.github.com>
2022-10-17 17:18:14 +0900

    비밀 보호

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const config = require("./config/key");
+
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017")
-  .then(() => console.log("MongoDB Connected"))
+  .connect(config.mongoURI)
+  .then(() => console.log("MongoDB Connected...."))
   .catch((err) => console.log(err));
+
+const bodyParser = require("body-parser");
+
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json
+app.use(bodyParser.json());
 
 // mongoose
 //   .connect(
@@ -37,7 +48,19 @@ mongoose
 // });
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! ~~ 안녕하세요?");
+});
+
+app.post("/register", (req, res) => {
+  // 회원가입에 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
+  user.save((err, doc) => {
+    if (err) return res.json({ sucess: false, err });
+    return res.status(200).json({
+      sucess: true,
+    });
+  });
 });
 
 app.listen(port, () => {
